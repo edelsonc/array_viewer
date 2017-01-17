2828c2c543ae1bc56c877bfd13c1a557551c5162
edelsonc <Charles.Edelson12@ncf.edu>
2017-01-16 23:36:24 -0500

    Initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='array_viewer',
+      version='0.1',
+      description='A simple numpy 2D array viewer',
+      url='https://github.com/edelsonc/array_viewer',
+      author='edelsonc',
+      author_email='cje5555@bellsouth.net',
+      license='MIT',
+      packages=['array_viewer'],
+      zip_safe=False
+)
