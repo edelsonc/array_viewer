44b9bfa43028292c717fc0495e220da6ca7d7d64
edelsonc <Charles.Edelson12@ncf.edu>
2017-01-17 23:33:45 -0500

    Added some basic funtionallity to Viewer class     * added selectable colormaps     * grey scale for booleans     * added colorbar     * removed axes     * added a block option on view

--- pyctarr/viewer_class.py
+++ pyctarr/viewer_class.py
@@ -10,7 +10,7 @@ class Viewer(object):
     view -- displays an heatmap of the array for a specified length of time
     update -- updates the viewers array and then the image
     """
-    def __init__(self, array, disp_time=1):
+    def __init__(self, array, disp_time=1, colormap="viridis"):
         """
         Instantiates a viewer object with a given array and display time
     
@@ -18,23 +18,38 @@ class Viewer(object):
         ---------
         array -- a 2D numpy array to be viewed later
         disp_time -- desired display time for each plot; plot refresh speed
+        colormap -- colormap used for heatmap; for list of colormaps see
+            http://matplotlib.org/examples/color/colormaps_reference.html
         """
+        self.__is2D(array)
         self.array = array
         self.disp_time = disp_time
         self.fig, self.ax, self.im = None, None, None
+        if self.array.dtype == 'bool':
+            self.colormap = "Greys"  # grey scale for boolean data type
+        else:
+            self.colormap = colormap
 
-    def view(self):
+    def view(self, block=False):
         """
         Basic viewing function that creates a single heatmap with the original
         array.
+
+        Arguments
+        ---------
+        block -- boolean for if script should be blocked by image
         """
         if None in (self.fig, self.ax, self.im):
             self.fig, self.ax = pyplot.subplots()
-            self.im = self.ax.imshow(self.array, interpolation='nearest')        
+            self.im = self.ax.imshow(self.array, cmap=self.colormap,
+                interpolation='nearest')
+            pyplot.colorbar(self.im)
         else:
             self.im.set_array(self.array)
-
-        pyplot.draw()
+        
+        
+        self.ax.axis("off")
+        pyplot.show(block=block)
         pyplot.pause(self.disp_time)
 
     def update(self, narray):
@@ -45,5 +60,17 @@ class Viewer(object):
         ---------
         narray -- new 2D numpy array
         """
+        self.__is2D(narray)
         self.array = narray
         self.view()
+
+    @staticmethod
+    def __is2D(array):
+        """Class method to check array isn't larger than 2D"""
+        if len(array.shape) > 2:
+            raise ValueError("Array is more than 2 dimensions")
+
+import numpy
+x = numpy.array([[1,2,3],[4,5,6]])
+viewer = Viewer(x)
+viewer.view()
